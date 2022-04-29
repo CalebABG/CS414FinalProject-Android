3089413287601d3329084eeb03084a729339e519
Caleb A. Bostic-Gardner <ca.bosticgardner@my.ccsu.edu>
2022-04-29 04:19:41 -0400

    Update packet data structure

--- app/src/main/java/com/example/cs414finalprojectandroid/bluetooth/ArduinoPacket.kt
+++ app/src/main/java/com/example/cs414finalprojectandroid/bluetooth/ArduinoPacket.kt
@@ -3,26 +3,12 @@ package com.example.cs414finalprojectandroid.bluetooth
 import com.example.cs414finalprojectandroid.Utilities.toByteArray
 import java.util.zip.CRC32
 
-/*
-    Index      0:       Packet START
-    Index      1:       Packet ID
-    Index      2:       Packet ACK Byte
-    Index      3:       Packet Data Sign (01 = Sensor X is negative; 02 = sensor Y is negative; 03 = both negative, 00 = neither negative)
-    Index      4:       Packet Sensor X
-    Index      5:       Packet Sensor Y
-    Index      6:       Packet END
-
-    Example Packet
-    Index:   0     1     2     3     4     5     6
-             01    D7    01    FF    FF    FF    04
-*/
-
 class ArduinoPacket {
     companion object {
         val crc32: CRC32 = CRC32()
 
         // Length in bytes
-        const val DATA_LENGTH: Short = 0x40
+        const val DATA_LENGTH: Short = 0xA
 
         // Start and End packet bytes
         const val PACKET_START: Byte = 0x1
@@ -48,33 +34,27 @@ class ArduinoPacket {
     private var dataLength: Short = 0x0
     private var data: ByteArray = ByteArray(DATA_LENGTH.toInt())
 
+    private fun addBytesToList(byteArray: ByteArray, mutableList: MutableList<Byte>) {
+        for (byte in byteArray)
+            mutableList.add(byte)
+    }
+
     fun build(): ByteArray {
-        val packetLength = 75
-        val packet = ByteArray(packetLength)
+        val packet: MutableList<Byte> = mutableListOf()
 
-        packet[0] = PACKET_START
-        packet[packetLength - 1] = PACKET_END
+        packet.add(PACKET_START)
 
-        val idBytes = id.toByteArray()
-        packet[1] = idBytes[0]
-        packet[2] = idBytes[1]
+        addBytesToList(id.toByteArray(), packet)
+        addBytesToList(crc.toByteArray(), packet)
 
-        val crcBytes = crc.toByteArray()
-        packet[3] = crcBytes[0]
-        packet[4] = crcBytes[1]
-        packet[5] = crcBytes[2]
-        packet[6] = crcBytes[3]
+        packet.add(ack)
 
-        packet[7] = ack
+        addBytesToList(dataLength.toByteArray(), packet)
+        addBytesToList(data, packet)
 
-        val dataLengthBytes = dataLength.toByteArray()
-        packet[8] = dataLengthBytes[0]
-        packet[9] = dataLengthBytes[1]
+        packet.add(PACKET_END)
 
-        for (i in 0 until DATA_LENGTH)
-            packet[i + 10] = data[i]
-
-        return packet
+        return packet.toByteArray()
     }
 
     fun setHeader(packetId: Short): ArduinoPacket {
@@ -84,10 +64,14 @@ class ArduinoPacket {
     }
 
     fun setCRC32(): ArduinoPacket {
-        crc32.update(id.toInt())
-        crc32.update(ack.toInt())
-        crc32.update(dataLength.toInt())
-        crc32.update(data)
+        val packet: MutableList<Byte> = mutableListOf()
+
+        addBytesToList(id.toByteArray(), packet)
+        packet.add(ack)
+        addBytesToList(dataLength.toByteArray(), packet)
+        addBytesToList(data, packet)
+
+        crc32.update(packet.toByteArray())
 
         crc = crc32.value.toInt()
         crc32.reset()
